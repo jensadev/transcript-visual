b770f7995d6a5d11ad743cf5c1d830e263b2f36a
Jens Andreasson <jens.andreasson@ntig.se>
2024-03-28 12:56:35 +0100

    Refactor code and update CSS styles

--- app.py
+++ app.py
@@ -5,34 +5,33 @@ from datetime import datetime
 import os
 import rcssmin
 
-env = Environment(
-    loader=PackageLoader("app"),
-    autoescape=select_autoescape()
-)
+env = Environment(loader=PackageLoader("app"), autoescape=select_autoescape())
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', required=True, help='Path to the CSV file')
+parser.add_argument("-f", "--file", required=True, help="Path to the CSV file")
 
 args = parser.parse_args()
 
 input_filename = args.file
 basename = os.path.splitext(os.path.basename(input_filename))[0]
-output_filename = f'output/{basename}.html'
+output_filename = f"output/{basename}.html"
 
-template = env.get_template('template.html')
+template = env.get_template("template.html")
 
-with open(args.file, 'r', encoding='utf-8') as file:
+with open(args.file, "r", encoding="utf-8") as file:
     reader = csv.reader(file)
     next(reader)
     # rendered_template = template.render({'rows': reader})
-    rows = [[datetime.strptime(row[0], '%H:%M')] + row[1:] for row in reader]
+    rows = [[datetime.strptime(row[0], "%H:%M")] + row[1:] for row in reader]
 
-    with open('style.css', 'r', encoding="utf-8") as file:
+    with open("style.css", "r", encoding="utf-8") as file:
         css = file.read()
         minified_css = rcssmin.cssmin(css)
 
-        rendered_template = template.render({'rows': rows, 'style': minified_css})
+        rendered_template = template.render(
+            {"title": output_filename, "rows": rows, "style": minified_css}
+        )
 
 
-with open(output_filename, 'w', encoding='utf-8') as html_file:
+with open(output_filename, "w", encoding="utf-8") as html_file:
     html_file.write(rendered_template)
