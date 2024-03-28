318c738e683f5ddb463b38ef4da8754c3ae240f7
Jens Andreasson <jens.andreasson@ntig.se>
2024-03-28 12:02:54 +0100

    Add output directory and generate HTML file dynamically

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import argparse
 import csv
 from jinja2 import Environment, PackageLoader, select_autoescape
 from datetime import datetime
+import os
 
 env = Environment(
     loader=PackageLoader("app"),
@@ -13,6 +14,10 @@ parser.add_argument('-f', '--file', required=True, help='Path to the CSV file')
 
 args = parser.parse_args()
 
+input_filename = args.file
+basename = os.path.splitext(os.path.basename(input_filename))[0]
+output_filename = f'output/{basename}.html'
+
 template = env.get_template('template.html')
 
 with open(args.file, 'r', encoding='utf-8') as file:
@@ -22,5 +27,5 @@ with open(args.file, 'r', encoding='utf-8') as file:
     rows = [[datetime.strptime(row[0], '%H:%M')] + row[1:] for row in reader]
     rendered_template = template.render({'rows': rows})
 
-with open('output.html', 'w', encoding='utf-8') as html_file:
+with open(output_filename, 'w', encoding='utf-8') as html_file:
     html_file.write(rendered_template)
